f9dc3f60c24ca55d338cc6d5ff1a68de46bdbe98
Impersy <duddjqkrtk2@naver.com>
2024-05-12 22:45:59 +0900

    prototype_001

--- board/urls.py
+++ board/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 
 from . import views
@@ -7,4 +6,9 @@ app_name = 'board'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('list/', views.list, name='list'),
+    path('read/<int:id>/', views.read, name='read'),
+    path('regist/', views.regist, name='regist'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('remove/<int:id>/', views.remove, name='remove'),
 ]
